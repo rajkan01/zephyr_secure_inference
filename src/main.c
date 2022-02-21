9190e83017fe91dd4c485b732bde6836ac5c6c3d
Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>
2022-02-21 12:47:45 +0000

    sample: confidential_ai: Add key context
    Add key context data structure to maintain general information about all three
    key types (Device Client TLS, COSE Sign, COSE Encrypt) like the key label, key
    id, and key status (Provisioning done or Key generated).
    
    Signed-off-by: Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>

--- src/main.c
+++ src/main.c
@@ -70,6 +70,74 @@ err:
 	return status;
 }
 
+void key_context_init(key_context_t *ctx)
+{
+	uint8_t public_key[65] = { 0 }; /* EC public key = 65 bytes. */
+	size_t public_key_len = sizeof(public_key);
+	psa_status_t status;
+	/* String format output config. */
+	struct sf_hex_tbl_fmt fmt = {
+		.ascii = true,
+		.addr_label = true,
+		.addr = 0
+	};
+
+	ctx[0].key_id = CLIENT_TLS;
+	strcpy(ctx[0].label, "Device Client TLS");
+	status = al_psa_status(
+		psa_huk_get_pubkey(&ctx[0].key_id,
+				   public_key,
+				   public_key_len),
+		__func__);
+	if (status != PSA_SUCCESS) {
+		LOG_ERR("Failed to export the_public_key");
+		goto err;
+	} else {
+		ctx[0].status = KEY_GEN;
+	}
+
+	printf("%s public key\n", ctx[0].label);
+	sf_hex_tabulate_16(&fmt, public_key, public_key_len);
+
+	ctx[1].key_id = C_SIGN;
+	strcpy(ctx[1].label, "Device COSE SIGN");
+	status = al_psa_status(
+		psa_huk_get_pubkey(&ctx[1].key_id,
+				   public_key,
+				   public_key_len),
+		__func__);
+	if (status != PSA_SUCCESS) {
+		LOG_ERR("Failed to export the_public_key");
+		goto err;
+	} else {
+		ctx[1].status = KEY_GEN;
+	}
+
+	printf("%s public key\n", ctx[1].label);
+	sf_hex_tabulate_16(&fmt, public_key, public_key_len);
+
+	ctx[2].key_id = C_ENCRYPT;
+	strcpy(ctx[2].label, "Device COSE ENCRYPT");
+	status = al_psa_status(
+		psa_huk_get_pubkey(&ctx[2].key_id,
+				   public_key,
+				   public_key_len),
+		__func__);
+	if (status != PSA_SUCCESS) {
+		LOG_ERR("Failed to export the_public_key");
+		goto err;
+	} else {
+		ctx[2].status = KEY_GEN;
+	}
+
+	printf("%s public key\n", ctx[2].label);
+	sf_hex_tabulate_16(&fmt, public_key, public_key_len);
+
+err:
+	/* Dump any queued log messages, and wait for system events. */
+	al_dump_log();
+}
+
 void main(void)
 {
 	psa_status_t status;
@@ -95,6 +163,8 @@ void main(void)
 		.addr = 0
 	};
 
+	key_context_init(get_key_context());
+
 #if CONFIG_SECURE_INFER_SHELL_CMD_SUPPORT
 	// Generate UUID
 	status = al_psa_status(
@@ -116,8 +186,6 @@ void main(void)
 		goto err;
 	}
 
-	LOG_INF("Exported public key:");
-	sf_hex_tabulate_16(&fmt, public_key, public_key_len);
 
 	for (int i = 1; i <= 20; i++) {
 

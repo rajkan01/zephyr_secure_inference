4dc7ce65236157851e8cf25cf6cd6adbdd820a68
David Brown <david.brown@linaro.org>
2022-02-25 14:35:12 -0700

    cloud: Add skeleton for Azure shell commands
    These commands will control the Azure cloud connection.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

---
+++ src/shell/cmd_azure.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <shell/shell.h>
+#include <logging/log.h>
+
+LOG_MODULE_DECLARE(app, CONFIG_LOG_DEFAULT_LEVEL);
+
+#ifdef CONFIG_AZURE_SHELL_CMD_SUPPORT
+
+static int
+cmd_azure_status(const struct shell *shell, size_t argc, char **argv)
+{
+	shell_print(shell, "No Azure support compiled in");
+
+	return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_cmd_azure,
+	/* Status */
+	SHELL_CMD(status, NULL, "Azure connection status", cmd_azure_status),
+	SHELL_SUBCMD_SET_END
+	);
+
+SHELL_CMD_REGISTER(azure, &sub_cmd_azure, "Azure commands", NULL);
+
+#endif /* CONFIG_AZURE_SHELL_CMD_SUPPORT */

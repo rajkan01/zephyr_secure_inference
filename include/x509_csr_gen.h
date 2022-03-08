93be0e29b84dad72ded2b35037c102e413c0c65b
Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>
2022-03-08 10:41:47 +0000

    sample: X509: Add X509 CSR generation support
    MbedTLS is already provided API for generating CSR, but the problem is
    accessing the PSA crypto secure service to sign the CSR hash value from NS-side
    as secure inferencing, all the keys are generated and maintained under HUK
    secure partition because of that NS-side cant directly use PSA crypto service
    for a sign the hash value so added similar implementation of mbedTLS except it
    request newly added TFM huk hash sign secure service for calculating the hash
    value of CSR payload and sign it.
    
    Signed-off-by: Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>

---
+++ include/x509_csr_gen.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2022 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef X509_CSR_GEN_H
+#define X509_CSR_GEN_H
+
+#include <logging/log.h>
+#include "mbedtls/x509_csr.h"
+#include "mbedtls/asn1write.h"
+#include "mbedtls/error.h"
+#include "mbedtls/oid.h"
+
+#if defined(MBEDTLS_PEM_WRITE_C)
+#include "mbedtls/pem.h"
+#endif
+
+#if defined(MBEDTLS_PLATFORM_C)
+#include "mbedtls/platform.h"
+#else
+#include <stdlib.h>
+#define mbedtls_calloc    calloc
+#define mbedtls_free      free
+#endif
+
+#include "key_mgmt.h"
+#include "tfm_partition_huk.h"
+
+#define X509_CSR_PEM_BEGIN          "-----BEGIN CERTIFICATE REQUEST-----\n"
+#define X509_CSR_PEM_END             "-----END CERTIFICATE REQUEST-----\n"
+#define X509_CSR_SUB_ORG "O=Linaro" /**< CSR subject name of Organization */
+
+/** Inidicates CSR format. */
+typedef enum {
+	CSR_PEM_FORMAT = 0,     /**< CSR in PEM format */
+	CSR_JSON_FORMAT,        /**< CSR in JSON format */
+	CSR_PEM_JSON_FORMAT,    /**< CSR in PEM & JSON format */
+	CSR_NONE,
+} x509_csr_fmt_t;
+
+/**
+ * @brief Generates device certificate signing request (CSR) using Mbed TLS
+ * X.509 and TF-M HUK CSR service.
+ *
+ * @param key_idx     Key context index.
+ * @param csr         Pointer to the buffer to store CSR.
+ * @param csr_len     The size in bytes of @p csr.
+ * @param uuid        Unique UUID.
+ * @param uuid_size   UUID size in bytes.
+ *
+ * @return psa_status_t
+ */
+psa_status_t x509_csr_generate(const km_key_idx_t key_idx,
+			       unsigned char *csr,
+			       size_t csr_len,
+			       unsigned char *uuid,
+			       size_t uuid_size);
+
+/**
+ * @brief Encode CSR to JSON format.
+ *
+ * @param csr                Pointer to the buffer of stored CSR.
+ * @param csr_json_buff      Pointer to the buffer to store CSR encoded JSON.
+ * @param csr_json_buff_len  The size in bytes of @p csr_json_buff.
+ *
+ * @return psa_status_t
+ */
+psa_status_t x509_csr_json_encode(unsigned char *csr,
+				  unsigned char *csr_json_buff,
+				  size_t csr_json_buff_len);
+
+#endif  /* X509_CSR_GEN_H */

743f4782e6f769d59e03324024dd54ec171ed6c8
Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>
2022-03-08 10:41:16 +0000

    sample: huk: Add huk hash sign service API
    Add the new tfm huk hash sign secure service API, which is used by NS-Side
    applications to request the service.
    
    Signed-off-by: Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>

--- src/tfm_partition_huk.c
+++ src/tfm_partition_huk.c
@@ -71,3 +71,41 @@ psa_status_t psa_huk_get_uuid(void *uuid,
 
 	return status;
 }
+
+psa_status_t psa_huk_hash_sign(psa_key_id_t *key_id,
+			       uint8_t *csr_data,
+			       size_t csr_data_size,
+			       uint8_t *sig,
+			       size_t sig_size,
+			       size_t *sig_len)
+{
+	psa_status_t status;
+	psa_handle_t handle;
+
+	psa_invec in_vec[] = {
+		{ .base = key_id, .len = sizeof(psa_key_id_t) },
+		{ .base = csr_data, .len = csr_data_size },
+	};
+
+	psa_outvec out_vec[] = {
+		{ .base = sig, .len = sig_size },
+		{ .base = sig_len, .len = sizeof(size_t) },
+	};
+
+	handle = psa_connect(TFM_HUK_HASH_SIGN_SID,
+			     TFM_HUK_HASH_SIGN_VERSION);
+	if (!PSA_HANDLE_IS_VALID(handle)) {
+		return PSA_ERROR_GENERIC_ERROR;
+	}
+
+	status = psa_call(handle,
+			  PSA_IPC_CALL,
+			  in_vec,
+			  IOVEC_LEN(in_vec),
+			  out_vec,
+			  IOVEC_LEN(out_vec));
+
+	psa_close(handle);
+
+	return status;
+}

582bb987a9239e24f9ab648bc0a024cc00bc7910
Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>
2022-03-22 16:53:14 +0000

    sample: huk: Add CBOR encode API for inference value encode to CBOR
    Add the new CBOR encode API to encode the inference value only to CBOR
    format.
    
    Signed-off-by: Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>

--- tfm_secure_partitions/tfm_huk_deriv_srv/cbor_cose_api.h
+++ tfm_secure_partitions/tfm_huk_deriv_srv/cbor_cose_api.h
@@ -14,11 +14,42 @@
 extern "C" {
 #endif
 
+/**
+ * \brief CBOR encode and sign the encoded inference value using private key of
+ * the given key handle.
+ *
+ * \param[in]   key_handle                Key handle.
+ * \param[in]   inf_val                   The inference input value.
+ * \param[out]  inf_val_encoded_buf       Buffer to which encoded data *
+ *                                        is written into.
+ * \param[in]   inf_val_encoded_buf_size  Size of inf_val_encoded_buf in bytes.
+ * \param[out]  inf_val_encoded_buf_len   Encoded and signed payload len in
+ *                                        bytes.
+ *
+ * \return Returns error code as specified in \ref psa_status_t
+ */
 psa_status_t tfm_cose_encode_sign(psa_key_handle_t key_handle,
 				  float inf_val,
 				  uint8_t *inf_val_encoded_buf,
 				  size_t inf_val_encoded_buf_size,
 				  size_t *inf_val_encoded_buf_len);
+
+/**
+ * \brief Encoding the inference value in CBOR format.
+ *
+ * \param[in]   inf_val                   The inference input value.
+ * \param[out]  inf_val_encoded_buf       Buffer to which encoded data
+ *                                        is written into.
+ * \param[in]   inf_val_encoded_buf_size  Size of inf_val_encoded_buf in bytes.
+ * \param[out]  inf_val_encoded_buf_len   Encoded and signed payload len in
+ *                                        bytes.
+ *
+ * \return Returns error code as specified in \ref psa_status_t
+ */
+psa_status_t tfm_cbor_encode(float inf_val,
+			     uint8_t *inf_val_encoded_buf,
+			     size_t inf_val_encoded_buf_size,
+			     size_t *inf_val_encoded_buf_len);
 
 #ifdef __cplusplus
 }

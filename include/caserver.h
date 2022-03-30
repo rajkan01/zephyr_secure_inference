3e45cd295e40f737f09bdc5f516afe30296c1ef0
David Brown <david.brown@linaro.org>
2022-03-30 12:00:56 -0600

    Add missing caserver.h header
    This is needed to invoke the CA server communication code.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

---
+++ include/caserver.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __CASERVER_H__
+#define __CASERVER_H__
+
+int caserver_cr(unsigned char *req, size_t req_len);
+
+#endif /* not __CASERVER_H__ */

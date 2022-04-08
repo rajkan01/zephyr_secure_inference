9324390c24fbaa1e5b832eec4a78817bc1f44281
Kevin Townsend <kevin.townsend@linaro.org>
2022-04-08 13:15:57 +0400

    cfg: Update app cfg structure for cert/key mgmnt
    Updates the config struct to better encapsulate the required X.509
    certificates and private keys required by the system.
    
    Signed-off-by: Kevin Townsend <kevin.townsend@linaro.org>

--- include/util_app_cfg.h
+++ include/util_app_cfg.h
@@ -14,9 +14,14 @@ extern "C" {
 #endif
 
 /**
+ * @brief Size in bytes of the bootstrap private key.
+ */
+#define APP_CFG_CA_BOOTSTRAP_KEYSZ (121)
+
+/**
  * @brief The struct used to persist config data to secure storage.
  *
- * The first 6 bytes of this struct should remain consistent in any future
+ * The first 8 bytes of this struct should remain consistent in any future
  * firmware updates, since they can be used to identify to layout of the rest
  * of the struct in cases where config data version management becomes
  * a necessity.
@@ -28,54 +33,149 @@ struct cfg_data {
 	uint32_t magic;
 
 	/**
-	 * @brief The version number for the stored config record.
-	 *
-	 * This number should be incremented any time the config_data struct
-	 * definition changes to allow version management of config data at
-	 * the application level.
+	 * @brief 32-bit version number data. The first 16 bits represent the
+	 *        version of the struct (@ref ver_major and @ref ver_minor). The
+	 *        second 16 bits are a counter indicating how many times the struct
+	 *        content has been updated (@ref counter).
 	 */
-	uint16_t version;
+	union {
+		struct {
+			/**
+			 * @brief The major version number of the config record,
+			 *        identifying how the rest of this struct should be parsed.
+			 *
+			 * This number should be incremented any time the cfg_data struct
+			 * definition changes in a significant manner to allow version
+			 * management of config data at the application level, and handling
+			 * of different struct definitions across released firmware images.
+			 */
+			uint8_t major;
+
+			/**
+			 * @brief The minor version number of the config record,
+			 *        identifying how the rest of this struct should be parsed.
+			 *
+			 * This number should be incremented any time the cfg_data struct
+			 * definition changes to allow version management of config data at
+			 * the application level, and handling of different struct 
+			 * definitions across released firmware images.
+			 */
+			uint8_t minor;
+
+			/**
+			 * @brief The revision number for the data stored in the config
+			 *        record.
+			 *
+			 * This number should be incremented any time the value(s) assigned
+			 * to the cfg_data struct change.
+			 */
+			uint16_t counter;
+		} version;
+
+		/** 32-bit representation of all version bits. */
+		uint32_t version_bits;
+	};
 
 	/**
-	 * @brief Device TLS certificate (KEY_ID_CLIENT_TLS).
+	 * @brief X.509 certificates associated with this device.
 	 */
-	km_x509_cert_t cert_device_tls;
+	struct {
+		/**
+		 * @brief Device TLS certificate (KEY_ID_CLIENT_TLS).
+		 * 
+		 * This client certificate has been signed by the CA and enables mutual
+		 * authentication over TLS to an MQTT broker or custom TLS server. It
+		 * is unique to this device, containing the device UUID and a unique
+		 * serial number assigned by the CA.
+		 */
+		km_x509_cert_t client_tls;
 
-	/**
-	 * @brief COSE signing certificate (KEY_ID_C_SIGN).
-	 */
-	km_x509_cert_t cert_device_cose_sign;
+		/**
+		 * @brief COSE signing certificate (KEY_ID_C_SIGN).
+		 * 
+		 * Contains the certificate associated with the signing key used by
+		 * this device to sign COSE SIGN1 payloads.
+		 */
+		km_x509_cert_t client_cose_sign1;
 
-	/**
-	 * @brief COSE client encryption certificate (KEY_ID_C_ENCRYPT).
-	 */
-	km_x509_cert_t cert_device_cose_encrypt;
+		/**
+		 * @brief COSE client encryption certificate (KEY_ID_C_ENCRYPT).
+		 * 
+		 * Contains the certificate associated with the device encryption key
+		 * used by this device to encrypt COSE ENCRYPT0 payloads.
+		 */
+		km_x509_cert_t client_cose_encrypt0;
 
-	/**
-	 * @brief Signing certificate for the device provisioning CA.
-	 *
-	 * The certificate provided by the CA to verify signatures on CA-issued
-	 * certificates.
-	 */
-	km_x509_cert_t cert_ca_sign;
+		/**
+		 * @brief Client bootstrap certificate for device auth to the CA when
+		 *        registering new certificates.
+		 *
+		 * This client certificate is required to authenticate devices on the
+		 * CA during the bootstrapping process in the factory. This enables
+		 * limiting certificate registration to devices containing this
+		 * certificate and associated private key. This certificate has been
+		 * signed with the CA key.
+		 * 
+		 * With LinaroCA this will be the contents of 'bootstrap_crt.txt'.
+		 */
+		km_x509_cert_t ca_bootstrap;
 
-	/**
-	 * @brief TLS certificate for the device provisioning CA.
-	 *
-	 * The certificate used to securely connect to the REST API (etc.) on
-	 * the CA to register a new device.
-	 */
-	km_x509_cert_t cert_ca_tls;
+		/**
+		 * @brief Signing certificate for the device provisioning CA.
+		 *
+		 * The certificate provided by the CA to verify signatures on CA-issued
+		 * certificates.
+		 */
+		km_x509_cert_t ca_sign;
 
-	/**
-	 * @brief TLS certificate for the MQTT broker.
-	 *
-	 * The certificate used to securely connect to the MQTT broker. This is
-	 * usually the CA that generated the MQTT broker's server certificate
-	 * (digicert for Azure IoT Hub, etc.).
-	 */
-	km_x509_cert_t cert_mqtt_tls;
+		/**
+		 * @brief Server TLS certificate for the device provisioning CA,
+		 *        required to connect to the REST API over TLS.
+		 *
+		 * The certificate provided by the CA to verify signatures on CA-issued
+		 * certificates.
+		 */
+		km_x509_cert_t ca_host_tls;
+
+		/**
+		 * @brief COSE server encryption certificate.
+		 * 
+		 * Contains the certificate associated with the server's encryption key
+		 * used by this device to encrypt COSE ENCRYPTO0 payloads. Both the
+		 * client private key and the recipient server's public key are
+		 * required to generate the shared secret used to encrypt COSE
+		 * ENCRYPT0 payloads.
+		 */
+		km_x509_cert_t server_cose_encrypt0;
+
+		/**
+		 * @brief TLS certificate for the MQTT broker.
+		 *
+		 * The certificate used to securely connect to the MQTT broker. This is
+		 * usually the CA that generated the MQTT broker's server certificate
+		 * (digicert for Azure IoT Hub, etc.).
+		 */
+		km_x509_cert_t mqtt_tls;
+	} certs;
+
+	struct {
+		/**
+		 * @brief Client bootstrap private key associated with
+		 *        @ref cert_ca_bootstrap certificate.
+		 * 
+		 * With LinaroCA this will be the contents of 'bootstrap_key.txt'.
+		 */
+		uint8_t ca_bootstrap[APP_CFG_CA_BOOTSTRAP_KEYSZ];
+	} keys;
 };
+
+/**
+ * @brief Display the config data via printk.
+ * 
+ * @param p_cfg_data Pointer to the cfg_data struct where the config data to
+ *        display is stored.
+ */
+void cfg_print_data(struct cfg_data *p_cfg_data);
 
 /**
  * @brief Creates a new config record in secure storage.
@@ -83,6 +183,19 @@ struct cfg_data {
  * @return #PSA_SUCCESS on success, otherwise a appropriate psa_status_t code.
  */
 psa_status_t cfg_create_data(void);
+
+/**
+ * @brief Updates the stored config record in secure storage.
+ * 
+ * This will automatically increment the config counter field every time the
+ * record is persisted.
+ * 
+ * @param p_cfg_data Pointer to the cfg_data struct where the config data to
+ *        persist is stored.
+ * 
+ * @return psa_status_t 
+ */
+psa_status_t cfg_update_data(struct cfg_data *p_cfg_data);
 
 /**
  * @brief Attempts to load the config record from secure storage. If the
--- src/util_app_cfg.c
+++ src/util_app_cfg.c
@@ -21,22 +21,35 @@ LOG_MODULE_DECLARE(app, CONFIG_LOG_DEFAULT_LEVEL);
  * @brief Default config settings. These settings will be used when a new
  *        config record is created, or when the config record is reset.
  */
-static struct cfg_data cfg_data_dflt = {
+static const struct cfg_data cfg_data_dflt = {
 	.magic = 0x55CFDA7A,
-	.version = 1,
-	.cert_device_tls = { .sz = 0, .cert = { 0 } },
-	.cert_device_cose_sign = { .sz = 0, .cert = { 0 } },
-	.cert_device_cose_encrypt = { .sz = 0, .cert = { 0 } },
-	.cert_ca_sign = { .sz = 0, .cert = { 0 } },
-	.cert_ca_tls = { .sz = 0, .cert = { 0 } },
-	.cert_mqtt_tls = { .sz = 0, .cert = { 0 } },
+	.version.major = 0,
+	.version.minor = 1,
+	.version.counter = 0,
+	.certs.client_tls = { .sz = 0, .cert = { 0 } },
+	.certs.client_cose_sign1 = { .sz = 0, .cert = { 0 } },
+	.certs.client_cose_encrypt0 = { .sz = 0, .cert = { 0 } },
+	.certs.ca_bootstrap = { .sz = 0, .cert = { 0 } },
+	.certs.ca_sign = { .sz = 0, .cert = { 0 } },
+	.certs.ca_host_tls = { .sz = 0, .cert = { 0 } },
+	.certs.server_cose_encrypt0 = { .sz = 0, .cert = { 0 } },
+	.certs.mqtt_tls = { .sz = 0, .cert = { 0 } },
+	.keys.ca_bootstrap = { },
 };
+
+void cfg_print_data(struct cfg_data *p_cfg_data)
+{
+	LOG_INF("app_cfg: magic:           0x%08X", p_cfg_data->magic);
+	LOG_INF("app_cfg: version.major:   %d", p_cfg_data->version.major);
+	LOG_INF("app_cfg: version.minor:   %d", p_cfg_data->version.minor);
+	LOG_INF("app_cfg: version.counter: %d", p_cfg_data->version.counter);
+}
 
 psa_status_t cfg_create_data(void)
 {
 	psa_status_t status;
 
-	LOG_INF("app_cfg: Creating new config file with UID 0x%llX",
+	LOG_INF("app_cfg: Creating default config file with UID 0x%llX",
 		(uint64_t)cfg_data_uid);
 
 	/*
@@ -49,6 +62,37 @@ psa_status_t cfg_create_data(void)
 	 */
 	status = psa_ps_set(cfg_data_uid, sizeof(cfg_data_dflt),
 			    (void *)&cfg_data_dflt, 0);
+	if (status) {
+		goto err;
+	}
+
+err:
+	return (status ? al_psa_status(status, __func__) : status);
+}
+
+psa_status_t cfg_update_data(struct cfg_data *p_cfg_data)
+{
+	psa_status_t status;
+
+	LOG_INF("app_cfg: Updating config file with UID 0x%llX",
+		(uint64_t)cfg_data_uid);
+	
+	/* Increment the counter value every write. */
+	/* ToDo: Check stored counter value to avoid malicious user rollback. */
+	/* ToDo: Check if we actually need to update via a diff. */
+	p_cfg_data->version.counter++;
+	LOG_INF("app_cfg: counter set to %d", p_cfg_data->version.counter);
+
+	/*
+	 * psa_ps_create can also be used here, which enables to the use of
+	 * the psa_ps_set_extended function, but this requires us to set a
+	 * maximum file size for resource allocation. Since the upper limit
+	 * isn't known at present, we opt here for the simpler psa_ps_set
+	 * call which also creates the secure storage record if necessary,
+	 * but precludes the use of psa_ps_set_extended.
+	 */
+	status = psa_ps_set(cfg_data_uid, sizeof(cfg_data_dflt),
+			    p_cfg_data, 0);
 	if (status) {
 		goto err;
 	}
